1720b63d04e8914da8ef187a0b1189e467f54c04
loh <catabekov@mail.ru>
2021-04-23 18:59:58 +0600

    perepisal)

---
+++ src/components/Contacts.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Contacts = () => {
+    return (
+        <div>
+            <h1>Contacts</h1>
+            <p>number: +996 55000000</p>
+            <p>mail: catabekov@mail.ru</p>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus saepe aliquam nam impedit totam officia repudiandae fugiat sit earum? Necessitatibus numquam perspiciatis qui delectus aliquam voluptas libero inventore nobis asperiores.
+                Necessitatibus a fugiat molestiae autem corrupti sed, optio voluptates cumque delectus labore quam, beatae perspiciatis natus, ab deleniti eligendi aspernatur nostrum quasi corporis impedit placeat. Aspernatur iure velit porro mollitia.
+            </p>
+        </div>
+    );
+};
+
+export default Contacts;
